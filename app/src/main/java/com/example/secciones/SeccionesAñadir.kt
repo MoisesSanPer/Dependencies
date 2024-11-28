a5f8d4bf506854ee33c515c66d2a44d65d1ead39
MoisesSanPer <moisessanper5@gmail.com>
2024-11-28 16:08:54 +0100

    Ya cmabio correctamente

--- app/src/main/java/com/example/secciones/SeccionesAñadir.kt
+++ app/src/main/java/com/example/secciones/SeccionesAñadir.kt
@@ -1,147 +1,115 @@
 package com.example.secciones
 
-import com.example.dependencias.R
-
-import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-var List = mutableListOf<String>("Seccion1","Seccion2","Seccion3")
-
 @Composable
 @Preview
-fun SeccionesListarPrincipal(modifier: Modifier=Modifier){
-    SeccionesListarContent(modifier)
+fun DependenciesScreenAñadir(modifier: Modifier = Modifier){
+    DependenciesContentAñadir(modifier)
 }
 
-
 @Composable
-private fun SeccionesListarContent(modifier: Modifier=Modifier)
+fun DependenciesContentAñadir(modifier: Modifier)
 {
-    Column {
-        HorizontalDivider()
-        Row {
-
-            TopBarWithMenuIcon( )
-        }
-        HorizontalDivider()
-        Box(
-            modifier = Modifier.fillMaxSize(),
-
+    //Esta mal puesto esto aqui solo lo he puesto para ver que escribe
+    var Nombre by remember { mutableStateOf("") }
+    var NombreCorto by remember { mutableStateOf("") }
+    var Descripcion by remember { mutableStateOf("") }
+    Box(modifier = modifier, contentAlignment = Alignment.Center) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "AÑADIR SECCION",
+                fontSize = 30.sp,
+                modifier = Modifier.padding(bottom = 23.dp)
             )
-        {
+            TextField(
+                value = Nombre,
+                onValueChange = { newNom -> Nombre = newNom },
+                label = { Text("Nombre") },
+                singleLine = true,
+                //Sacado de Internet he preguntado para saber tonalidades profesionales
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color(0xFFF1F1F1),
+                    focusedTextColor = Color.Black,
+                    focusedLabelColor = Color(0xFF6200EE)
+                )
+            )
+
+
+
+            Spacer(modifier.size(10.dp))
+
+            TextField(
+                value = NombreCorto,
+                onValueChange = { newNomCor -> NombreCorto = newNomCor },
+                label = { Text("Nombre Corto") },
+                singleLine = true,
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color(0xFFF1F1F1),
+                    focusedTextColor = Color.Black,
+                    focusedLabelColor = Color(0xFF6200EE)
+                )
+            )
+            Spacer(modifier.size(10.dp))
             Box(
-                modifier.padding(vertical = 6.dp)
+                modifier = modifier.size(300.dp)
             )
             {
-                ItemList(List)
+                OutlinedTextField(
+                    value = Descripcion,
+                    onValueChange = { newDesc -> Descripcion = newDesc  },
+                    label = { Text("Descripción") },
+                    modifier = Modifier.fillMaxSize(),
+                    maxLines = 3,
+                )
             }
-            FloatingActionButton(
-                onClick = { },
-                modifier = Modifier.align(Alignment.BottomEnd).padding(16.dp),
-                containerColor = Color.Black,
-                contentColor = Color.White,
-            ) {
-                Icon(Icons.Filled.Add,"Añadir Dependencia")
-            }
-        }
+            Spacer(modifier.size(8.dp))
+            TextButton(
+                onClick = {},
+                colors = ButtonDefaults.textButtonColors(
+                    containerColor = Color.Green,
+                    contentColor = Color.DarkGray
+                ),
+                shape = RoundedCornerShape(8.dp)
 
-    }
-}
-
-@Composable
-fun ItemList(items: List<String>) {
-    LazyColumn(
-        modifier = Modifier.padding(horizontal = 16.dp)
-    ) {
-        items(items) { item ->
-            Row(
-                modifier = Modifier
-                    .border(
-                        width = 1.dp,
-                        color = Color.Black,
-                        shape = RoundedCornerShape(4.dp)
-                    )
-                    .padding(16.dp)
             ) {
                 Text(
-                    text = item,
-                    fontSize = 16.sp,
-                    modifier = Modifier.weight(1f) //Para que se me quede en la derecha
+                    text = "Añadir",
                 )
-                IconButton (
-                    onClick = {},
-
-                    ) {
-                    Icon(
-                        imageVector = Icons.Default.MoreVert,
-                        contentDescription = "Editar"
-                    )
-                }
             }
+
         }
+
+
     }
-}
-
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun TopBarWithMenuIcon(modifier: Modifier=Modifier) {
-    TopAppBar(
-        title = {   Text(
-            text = "Secciones",
-            fontSize = 20.sp,
-        ) },
-        navigationIcon = {
-
-            Icon(
-                imageVector = Icons.Default.Menu,
-                contentDescription = "Menú"
-            )
-        },
-        actions =
-        {
-            IconButton(
-                onClick = { }
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.filter),
-                    contentDescription = "Filtrar"
-                )
-            }
-            IconButton(
-                onClick = { }
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.sort),
-                    contentDescription = "Ordenar"
-                )
-            }
-        }
-    )
 }
